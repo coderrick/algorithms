bb71077e08cafb09f0b17cc860c2b947bc2f5bd7
coderrick <derrick.duncan@temple.edu>
2016-09-01 23:55:09 -0400

    Added graph class

---
+++ src/main/java/Graph.java
@@ -0,0 +1,14 @@
+class Vertex {
+    private ArrayList<Edge> neighborhood;
+    private String label;
+
+  public Vertex(String label){
+        this.label = label;
+        this.neighborhood = new ArrayList<Edge>();
+    }
+}
+
+public class Graph {
+
+
+}
