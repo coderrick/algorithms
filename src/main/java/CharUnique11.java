cc9dac09c924973665ce559d910c451497815d77
coderrick <derrick.duncan@temple.edu>
2016-10-13 01:46:32 +0200

    udates to 1.1

--- src/main/java/CharUnique11.java
+++ src/main/java/CharUnique11.java
@@ -3,12 +3,22 @@ Implement an algorithm to determine if a string has all unique characters. What
 if you cannot use additional data structures?
 */
 public class CharUnique11 {
-String input = "aardvark"
-    public void isUchar(input){
-        
+    static String s = "aardvark";
+    static char[] char_array = s.toCharArray();
+
+    public static void isUchar(char[] input){
+        for (int i = 0; i < input.length; i++){
+            System.out.println(input[i]);
+            for(int j = 0; j < input.length; j++){
+                if (input[i] == 'a'){
+                    System.out.println("grapes");
+                }
+            }
+            System.out.println("YES");
+        }
     }
 
     public static void main(String[] args) {
-        
+        isUchar(char_array);
     }
 }
---
+++ src/main/java/SubstringRotation18.java
@@ -0,0 +1,5 @@
+ma class className {
+    public static void main(String[] args) {
+        
+    }
+}
