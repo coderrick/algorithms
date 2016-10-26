ed9c8321a8332e28720caed86dc99bfa752679e3
coderrick <derrick.duncan@temple.edu>
2016-10-25 23:48:29 -0400

    update

--- src/main/misc/SampleGetRequest.java
+++ src/main/misc/SampleGetRequest.java
@@ -54,45 +54,4 @@ public class SampleGetRequest {
   System.out.println(response.toString());
 
  }
- 
- // HTTP Post request
- private void sendingPostRequest() throws Exception {
-
-  String url = "http://localhost:8080/JAXRSJsonCRUDExample/rest/countries";
-  URL obj = new URL(url);
-  HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-        // Setting basic post request
-  con.setRequestMethod("POST");
-  con.setRequestProperty("User-Agent", USER_AGENT);
-  con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-  con.setRequestProperty("Content-Type","application/json");
-
-  String postJsonData = "{\"id\":5,\"countryName\":\"USA\",\"population\":8000}";
-  
-  // Send post request
-  con.setDoOutput(true);
-  DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-  wr.writeBytes(postJsonData);
-  wr.flush();
-  wr.close();
-
-  int responseCode = con.getResponseCode();
-  System.out.println("\nSending 'POST' request to URL : " + url);
-  System.out.println("Post Data : " + postJsonData);
-  System.out.println("Response Code : " + responseCode);
-
-  BufferedReader in = new BufferedReader(
-          new InputStreamReader(con.getInputStream()));
-  String output;
-  StringBuffer response = new StringBuffer();
-
-  while ((output = in.readLine()) != null) {
-   response.append(output);
-  }
-  in.close();
-  
-  //printing result from response
-  System.out.println(response.toString());
- }
 }
